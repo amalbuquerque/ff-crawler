2001945347525c3c6bb7dd38352127513edd06d0
André Albuquerque <andre.m.de.albuquerque@gmail.com>
2016-02-24 13:22:06 +0000

    Previously only committed the .gitignore

---
+++ curler.py
@@ -0,0 +1,70 @@
+import ipdb
+import pycurl
+import re
+from lxml import etree
+from lxml.cssselect import CSSSelector
+from StringIO import StringIO
+
+CURRENCY_CODES = {
+        'EUR': 814,
+        'USD': 333,
+        'GBP': 402,
+        'CHF': 1,
+        'CNY': 220,
+        'JPY': 534
+        }
+
+class Curler(object):
+    """ classe que abstrai o acesso 'a ff boerse """
+    def __init__(self, currency, user = None, password = None, \
+             print_debug = False):
+        print('Debug: ' + str(print_debug))
+        self.user = user
+        self.password = password
+        self.currency = currency
+        self.debug = print_debug
+        self.curl = None
+        self.raw_response = None
+        self.response_buffer = StringIO()
+
+    def prt(self, msg):
+        if (self.debug):
+            print(msg)
+
+    def init_ajax_curl(self, init=True):
+        self.prt('Igniting the Curl')
+        c = pycurl.Curl()
+        url = str.format('http://en.boerse-frankfurt.de/Ajax/BondSearchResults?borrower=&marketsegment=&bondtype=&maturity=&yield=&rating=&exchange=&coupon=&currency={0}&count=100&p=1', CURRENCY_CODES[self.currency])
+        c.setopt(c.URL, url)
+        referer = str.format('http://en.boerse-frankfurt.de/bonds/bonds-finder?borrower=&marketsegment=&bondtype=&maturity=&yield=&rating=&exchange=&coupon=&currency={0}&count=100', CURRENCY_CODES[self.currency])
+        c.setopt(c.REFERER, referer)
+        c.setopt(c.USERAGENT, 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2464.0 Safari/537.36')
+
+        c.setopt(c.HTTPHEADER,
+                 ['Host: en.boerse-frankfurt.de',
+                  'Proxy-Connection: keep-alive',
+                  'Accept: */*',
+                  'X-Requested-With: XMLHttpRequest',
+                  'Accept-Language: en-US,en;q=0.8,pt;q=0.6'])
+
+        if (self.user != None and self.password != None):
+            c.setopt(c.USERPWD, self.user + ':' + self.password)
+            c.setopt(c.HTTPAUTH, c.HTTPAUTH_NTLM)
+
+        c.setopt(c.NOPROGRESS, 1)
+        c.setopt(c.WRITEFUNCTION, self.response_buffer.write)
+        if (init):
+            self.curl = c
+        return c
+
+    def perform(self):
+        self.init_ajax_curl()
+        if (self.curl != None):
+            url = self.curl.getinfo(self.curl.EFFECTIVE_URL)
+            self.prt('I will now perform the Curl request. URL=' + url)
+            self.curl.perform()
+            self.raw_response = self.response_buffer.getvalue()
+        self.prt('Content obtained:')
+        self.prt(self.raw_response)
+        return self.raw_response
+
---
+++ ffcrawler.py
@@ -0,0 +1,29 @@
+# coding: latin-1
+import argparse
+import ipdb
+import sys
+import exceptions
+from curler import Curler
+
+def main(*args):
+    try:
+        parser = argparse.ArgumentParser(description='FF Boerse Bond Crawler')
+
+        parser.add_argument('--currency', nargs='?', default='EUR', help='Currency to search for (default=EUR). Possible values: EUR, USD, GBP, CHF, CNY, JPY')
+        parser.add_argument('resultfile', nargs='?', default='resultBonds.csv', help='Result file (default=result<timestamp>.csv).')
+        parser.add_argument('--user', nargs='?', default = None, help='User to send if behind a proxy')
+        parser.add_argument('--password', nargs='?', default = None, help='Password to send if behind a proxy')
+        parser.add_argument('--debug', action='store_true', default = False, help='If True, prints everything (both content and debug messages)')
+        prog_args = parser.parse_args()
+        # Use as: prog_args.currency and prog_args.resultfile
+        # Use as: prog_args.user and prog_args.password
+        # ipdb.set_trace()
+        print('Init ended')
+    except:
+        raise Exception('Problem parsing the options passed')
+
+    curler = Curler(prog_args.currency, prog_args.user, prog_args.password, prog_args.debug)
+    curler.perform()
+
+if __name__ == '__main__':
+    sys.exit(main(*sys.argv))
